7e1014d3fd013543ef21ac866b58253b89da77be
VSFerreiras402 <VSFerreiras402@protonmail.com>
2025-01-23 23:03:22 -0400

    Correción de ruta de componentes

--- GestionTecnicos/obj/Debug/net9.0/GestionTecnicos.AssemblyInfo.cs
+++ GestionTecnicos/obj/Debug/net9.0/GestionTecnicos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestionTecnicos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22be33c430d0a945ad416f3c5233912060e28cec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9aaa71eca54d44a56a19fc90c7ce891e7e295a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestionTecnicos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestionTecnicos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
