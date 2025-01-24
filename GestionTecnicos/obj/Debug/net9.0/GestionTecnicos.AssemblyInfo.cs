eb02633222d12697a258816303cb754fa9e4e724
VSFerreiras402 <VSFerreiras402@protonmail.com>
2025-01-24 01:19:35 -0400

    Templates removidos

--- GestionTecnicos/obj/Debug/net9.0/GestionTecnicos.AssemblyInfo.cs
+++ GestionTecnicos/obj/Debug/net9.0/GestionTecnicos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestionTecnicos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb73690ee349b2a5917b8c31fa073dc30a9df426")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34ce7536bbd8be238458b20a5623d9f4749531fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestionTecnicos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestionTecnicos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
