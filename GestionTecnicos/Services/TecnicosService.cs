5e193d10abad0efb5f3e3098d641dbc3ea54e222
VSFerreiras402 <VSFerreiras402@protonmail.com>
2025-01-23 22:11:23 -0400

    Creado TecnicoService.cs

---
+++ GestionTecnicos/Services/TecnicosService.cs
@@ -0,0 +1,9 @@
+using GestionTecnicos.Models;
+using GestionTecnicos.DAL;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestionTecnicos.Services;
+
+public class TecnicosService(IDbContextFactory<Contexto> DbFactory){
+
+}
--- GestionTecnicos/obj/Debug/net9.0/GestionTecnicos.AssemblyInfo.cs
+++ GestionTecnicos/obj/Debug/net9.0/GestionTecnicos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestionTecnicos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4aad8fea98446d5a31992c8af2b47d32b057a670")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0be860d80139f32588a771e73a84d406460c872")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestionTecnicos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestionTecnicos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
