206021bdbc8d20a058cee8bcc36cba4d0e0b8b10
VSFerreiras402 <VSFerreiras402@protonmail.com>
2025-01-23 22:54:02 -0400

    Añadidos métodos al service

--- GestionTecnicos/Services/TecnicosService.cs
+++ GestionTecnicos/Services/TecnicosService.cs
@@ -1,9 +1,49 @@
 using GestionTecnicos.Models;
 using GestionTecnicos.DAL;
 using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace GestionTecnicos.Services;
 
 public class TecnicosService(IDbContextFactory<Contexto> DbFactory){
+
+    public async Task<List<Tecnicos>> Listar(Expression<Func<Tecnicos, bool>> criterio){
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Tecnicos.Where(criterio).AsNoTracking().ToListAsync();
+    }
+    public async Task<bool> Eliminar(int tecnicoId){
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Tecnicos.AsNoTracking().Where(t=>t.TecnicoId == tecnicoId).ExecuteDeleteAsync() > 0;
+    }
+    public async Task<Tecnicos?> Buscar(int tecnidoId){
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Tecnicos.FirstOrDefaultAsync(t => t.TecnicoId == tecnidoId);
+    }
+   private async Task<bool> Modificar(Tecnicos tecnico){
+    await using var contexto = await DbFactory.CreateDbContextAsync();
+    contexto.Update(tecnico);
+    return await contexto.SaveChangesAsync() > 0;
+
+   }
+   private async Task<bool> Insertar(Tecnicos tecnico){
+    await using var contexto = await DbFactory.CreateDbContextAsync();
+    contexto.Tecnicos.Add(tecnico);
+    return await contexto.SaveChangesAsync() > 0;
+
+   } 
+    private async Task<bool> Existe(int tecnicoId){
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Tecnicos.AnyAsync(t => t.TecnicoId == tecnicoId);
+    }
+
+
+    public async Task<bool> Guardar(Tecnicos tecnico) {
+        if (!await Existe(tecnico.TecnicoId)) {
+            return await Insertar(tecnico);
+        }
+        else{
+            return await Modificar(tecnico);
+        }
+    }
 
 }
