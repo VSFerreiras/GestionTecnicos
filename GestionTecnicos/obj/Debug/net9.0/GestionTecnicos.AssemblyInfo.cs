9616b584f53e6b785af85470721e36d6558da3d6
VSFerreiras402 <VSFerreiras402@protonmail.com>
2025-01-24 01:38:33 -0400

    Index correción

--- GestionTecnicos/obj/Debug/net9.0/GestionTecnicos.AssemblyInfo.cs
+++ GestionTecnicos/obj/Debug/net9.0/GestionTecnicos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestionTecnicos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fdab031f945e2653dd7b235efa15185fec0b434")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7e665459d2daa37f80e07eac61da8ed83cd4ec8")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestionTecnicos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestionTecnicos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
