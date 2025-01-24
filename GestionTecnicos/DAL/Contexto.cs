f8055df3b70772b3e2d55142781a0f46ab4ff69d
VSFerreiras402 <VSFerreiras402@protonmail.com>
2025-01-23 21:56:38 -0400

    Creacion de Contexto.cs

---
+++ GestionTecnicos/DAL/Contexto.cs
@@ -0,0 +1,10 @@
+using GestionTecnicos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestionTecnicos.DAL;
+
+public class Contexto : DbContext{
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+    public DbSet<Tecnicos> Tecnicos { get; set; }
+}
