f82ba8dbdc04c2d920eaa720ee2a4b787003d960
VSFerreiras402 <VSFerreiras402@protonmail.com>
2025-01-25 15:21:53 -0400

    Creado ClientesService.cs

--- GestionTecnicos/obj/Debug/net9.0/GestionTecnicos.AssemblyInfo.cs
+++ GestionTecnicos/obj/Debug/net9.0/GestionTecnicos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestionTecnicos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7e42b47d29fdcb301672c501efd917c6b96fb7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65de44d67ef32c72c99cb90840b46cd4031c11c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestionTecnicos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestionTecnicos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
